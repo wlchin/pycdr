60195124c2abbab983581423a16e0e6bc2dc1797
melvin_chin <weeloong.chin@outlook.com>
2022-01-13 02:22:56 +0000

    setup.py - include datafiles

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from setuptools import setup
 
 setup(name='cdr_py',
-      version='0.0.1',
+      version='0.0.2',
       packages=['pycdr',
                 'pycdr.test'],
       install_requires = ['anndata==0.7.6', 'scipy', 'dask-ml', 'tqdm', 'pandas==1.3.5', 'numpy==1.20.0', 'statsmodels'],
       python_requires='>3.5',
       author='WL Chin',
+      package_data={'': ['data/*.*']},
       author_email="melvin.chin@research.uwa.edu.au",
       url = "https://github.com/wlchin/pycdr",
       description = "CDR algorithm for multi-condition single-cell data"
